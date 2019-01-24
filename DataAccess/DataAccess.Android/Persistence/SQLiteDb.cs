849a4be8f9750b81dc67716a6d5d39117cd16bbf
Andressa Neumann <neumm.andressa@gmail.com>
2019-01-24 18:01:52 -0200

    first commit

---
+++ DataAccess/DataAccess.Android/Persistence/SQLiteDb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using DataAccess.Persistence;
+using SQLite;
+
+namespace DataAccess.Droid.Persistence
+{
+    public class SQLiteDb : ISQLiteDb
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
